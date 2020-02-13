c69cc703d27deec3addcf2d15d2b89c2de4326c2
Keoni Murray <keonimurray@Keonis-MacBook-Pro.local>
2020-02-13 14:11:24 -0800

    completed string alorithm challenge

--- Code/strings.py
+++ Code/strings.py
@@ -1,39 +1,65 @@
 #!python
 
 def contains(text, pattern):
-    """Return a boolean indicating whether pattern occurs in text."""
+    
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement contains here (iteratively and/or recursively)
 
+
+    if find_all_indexes(text, pattern):
+        return True
+    else:
+        return False
+
     
-
-
 def find_index(text, pattern):
-    """Return the starting index of the first occurrence of pattern in text,
-    or None if not found."""
+    
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_index here (iteratively and/or recursively)
 
+    list_of_occurances = find_all_indexes(text, pattern)
+    if list_of_occurances:
+        return list_of_occurances[0]
+    else:
+        return None
+
+
+
+
+        
+
 
 def find_all_indexes(text, pattern):
-    """Return a list of starting indexes of all occurrences of pattern in text,
-    or an empty list if not found."""
+    
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_all_indexes here (iteratively and/or recursively)
 
+    if not pattern:
+        return [i for i in range(len(text))]
 
-# def test_string_algorithms(text, pattern):
-#     found = contains(text, pattern)
-#     print('contains({!r}, {!r}) => {}'.format(text, pattern, found))
-#     # TODO: Uncomment these lines after you implement find_index
-#     index = find_index(text, pattern)
-#     print('find_index({!r}, {!r}) => {}'.format(text, pattern, index))
-#     # TODO: Uncomment these lines after you implement find_all_indexes
-#     indexes = find_all_indexes(text, pattern)
-#     print('find_all_indexes({!r}, {!r}) => {}'.format(text, pattern, indexes))
+
+    list_of_indexes = []
+    for i in range(len(text) - len(pattern) + 1):
+        for j in range(len(pattern)):
+            if text[i+j] != pattern[j]:
+                break
+        else:
+            list_of_indexes.append(i)
+    return list_of_indexes
+
+
+def test_string_algorithms(text, pattern):
+    found = contains(text, pattern)
+    print('contains({!r}, {!r}) => {}'.format(text, pattern, found))
+    # TODO: Uncomment these lines after you implement find_index
+    index = find_index(text, pattern)
+    print('find_index({!r}, {!r}) => {}'.format(text, pattern, index))
+    # TODO: Uncomment these lines after you implement find_all_indexes
+    indexes = find_all_indexes(text, pattern)
+    print('find_all_indexes({!r}, {!r}) => {}'.format(text, pattern, indexes))
 
 
 def main():
